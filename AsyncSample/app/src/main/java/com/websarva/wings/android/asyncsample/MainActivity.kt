059caff01b7b09f20ad1b6780976c47cec456fde
OmuRiceChang <maron_00626@yahoo.co.jp>
2022-08-20 12:04:13 +0900

    update AsyncSample folder

--- AsyncSample/app/src/main/java/com/websarva/wings/android/asyncsample/MainActivity.kt
+++ AsyncSample/app/src/main/java/com/websarva/wings/android/asyncsample/MainActivity.kt
@@ -2,10 +2,24 @@ package com.websarva.wings.android.asyncsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import java.net.URL
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
+import android.widget.TextView
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import androidx.core.os.HandlerCompat
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     companion object{
@@ -48,9 +62,75 @@ class MainActivity : AppCompatActivity() {
 
         return list
     }
-
+    @UiThread
     private fun receiveWeatherInfo(urlFull: String){
+        val handler = HandlerCompat.createAsync(mainLooper)
+        val backgroundReceiver = WeatherInfoBackgroundReceiver(handler,urlFull)
+        val executeService = Executors.newSingleThreadExecutor()
+        executeService.submit(backgroundReceiver)
 
+    }
+
+    private inner class WeatherInfoBackgroundReceiver(handler: Handler,url: String): Runnable {
+        private val _handler = handler
+        private val _url = url
+        @WorkerThread
+        override fun run() {
+            var result = ""
+            val url = URL(_url)
+            val con = url.openConnection() as? HttpURLConnection
+            con?.let {
+                try {
+                    it.connectTimeout = 1000
+                    it.readTimeout = 1000
+                    it.requestMethod = "GET"
+                    it.connect()
+                    val stream = it.inputStream
+                    result = is2String(stream)
+                    stream.close()
+                }
+                catch (ex: SocketTimeoutException){
+                    Log.w(DEBUG_TAG,"通信タイムアウト",ex)
+                }
+                it.disconnect()
+            }
+            val postExecutor = WeatherInfoPostExecutor(result)
+            _handler.post(postExecutor)
+        }
+        private fun is2String(stream: InputStream):String{
+            val sb = StringBuilder()
+            val reader = BufferedReader(InputStreamReader(stream,"UTF-8"))
+            var line = reader.readLine()
+            while(line != null){
+                sb.append(line)
+                line = reader.readLine()
+            }
+            reader.close()
+            return sb.toString()
+        }
+    }
+
+    private inner class WeatherInfoPostExecutor(result: String): Runnable{
+        private val _result = result
+        @UiThread
+        override fun run() {
+            Log.i(DEBUG_TAG,"WeatherInfoPostExecutor called1")
+            val rootJSON = JSONObject(_result)
+            val cityName = rootJSON.getString("name")
+            val coordJSON = rootJSON.getJSONObject("coord")
+            val latitude = coordJSON.getString("lat")
+            val longitude = coordJSON.getString("lon")
+            val weatherJSONArray = rootJSON.getJSONArray("weather")
+            val weatherJSON = weatherJSONArray.getJSONObject(0)
+            val weather = weatherJSON.getString("description")
+            val telop = "${cityName}の天気"
+            val desc = "現在は${weather}です。\n緯度は${latitude}度で経度は${longitude}度です。"
+            val tvWeatherTelop = findViewById<TextView>(R.id.tvWeatherTelop)
+            val tvWeatherDesc = findViewById<TextView>(R.id.tvWeatherDesc)
+            Log.i(DEBUG_TAG,"WeatherInfoPostExecutor called2")
+            tvWeatherTelop.text = telop
+            tvWeatherDesc.text = desc
+        }
     }
 
     private inner class ListItemClickListener: AdapterView.OnItemClickListener{
